13eed0ac278c231bebb4e8587d42af66eefc27cb
Pamela Leskar <pleskar@Pamelas-MacBook-Air-2.local>
2016-08-30 16:59:22 -0400

    PCI and Company Name MOTD

---
+++ cookbooks/motd/metadata.rb
@@ -0,0 +1,7 @@
+name 'motd'
+maintainer 'Pamela Leskar'
+maintainer_email 'pleskar@bitlancer.com'
+license 'All rights reserved'
+description 'Installs/Configures message of the day'
+version '0.1.0'
+
