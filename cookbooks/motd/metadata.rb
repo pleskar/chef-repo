9cdf4554574c9730900851b0fdb41a1437107e4f
Pamela Leskar <pleskar@Pamelas-MacBook-Air-2.local>
2016-08-30 17:01:30 -0400

    PCI and Company Name MOTD try number 2

--- cookbooks/motd/metadata.rb
+++ cookbooks/motd/metadata.rb
@@ -3,5 +3,6 @@ maintainer 'Pamela Leskar'
 maintainer_email 'pleskar@bitlancer.com'
 license 'All rights reserved'
 description 'Installs/Configures message of the day'
-version '0.1.0'
+version '0.1.1'
+depends 'pci'
 
