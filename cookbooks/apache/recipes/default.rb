ebb2afc208c2897f8edaf28b25a5e748290ef4e1
Pamela Leskar <pleskar@Pamelas-MacBook-Air-2.local>
2016-08-31 11:24:52 -0400

    Changed Line 27 Error and fixed spacing

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -10,8 +10,8 @@ end
 
 #Disable the default virtula host
 execute "mv /etc/httpd/conf.d/welcome.conf /etc/httpd/conf.d/welcome.conf.disabled" do
- only_if { File.exists?("/etc/httpd/confd/welcome.conf") }
- notifies :restart, "service[httpd]"
+  only_if { File.exists?("/etc/httpd/confd/welcome.conf") }
+  notifies :restart, "service[httpd]"
 end
 
 =begin
@@ -24,49 +24,49 @@ end
 
 #Iterate over the sites
 
-node ["apache"]["sites"].each do |site_name, site_data|
+node["apache"]["sites"].each do |site_name, site_data|
 #Set the document root variable
 document_root = "/srv/apache/#{site_name}"
  
- #Create the virtual hosts definition
- template "/etc/httpd/conf.d/#{site_name}.conf" do
-  source "custom.erb"
-  mode "0644"
-  variables(
-   :document_root => document_root,
-   :port => site_data["port"]
-  )
-  notifies :restart, "service[httpd]"
- end
+  #Create the virtual hosts definition
+  template "/etc/httpd/conf.d/#{site_name}.conf" do
+    source "custom.erb"
+    mode "0644"
+    variables(
+      :document_root => document_root,
+      :port => site_data["port"]
+     )
+   notifies :restart, "service[httpd]"
+  end
 
- #Create the appropriate apache directories
- directory "/srv/apache" do
-  owner "apache"
-  group "apache"
-  mode "0755"
- end
+  #Create the appropriate apache directories
+  directory "/srv/apache" do
+    owner "apache"
+    group "apache"
+    mode "0755"
+  end
 
- directory document_root do
-  owner "apache"
-  group "apache"
-  mode "0755"
-  recursive true
- end
+  directory document_root do
+    owner "apache"
+    group "apache"
+    mode "0755"
+    recursive true
+  end
 
- #Create each index.html page
- template "#{document_root}/index.html" do
-  source "index.html.erb"
-  owner "apache"
-  group "apache"
-  mode "0644"
-  variables(
-   :site_name => site_name,
-   :port => site_data["port"]
-  )
- end
+  #Create each index.html page
+  template "#{document_root}/index.html" do
+    source "index.html.erb"
+    owner "apache"
+    group "apache"
+    mode "0644"
+    variables(
+     :site_name => site_name,
+     :port => site_data["port"]
+    )
+  end
 end
 
 service "httpd" do
- action [ :enable, :start ]
+  action [ :enable, :start ]
 end
 
